ff319be2bb9a6836a541540fa5a3ceab812e0f3e
iamrealnurs <nurs567@yandex.ru>
2020-11-23 17:26:03 +0600

    Replace HttpResponse in views.py to render

--- django_courses/lesson_1/views.py
+++ django_courses/lesson_1/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello World!!! Страница номер 1.")
+    return render(request, '', ())
