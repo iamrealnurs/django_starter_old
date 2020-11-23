59d6d1eac78dbac685f5d7080974625d2ad2bfc5
iamrealnurs <nurs567@yandex.ru>
2020-11-23 17:11:25 +0600

    Added lesson_1 pathes to urlpatterns of django_courses/urls.py

--- django_courses/django_courses/urls.py
+++ django_courses/django_courses/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('lesson_1/', include('lesson_1.urls')),
     path('admin/', admin.site.urls),
 ]
