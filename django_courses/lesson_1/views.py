2f518f54ea30c0f6d17b8f4f80470d4a660abe03
iamrealnurs <nurs567@yandex.ru>
2020-11-23 17:40:01 +0600

    Refactor of views.py

--- django_courses/lesson_1/views.py
+++ django_courses/lesson_1/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'lesson_1/templates/index.html', {})
+    return render(request, 'index.html')
